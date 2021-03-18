dc198ccd8bcf344f9e84450e9860109c3b18eda8
montehurd <mhurd@wikimedia.org>
2021-03-18 13:18:56 -0700

    fix project search not accounting for parent projects

--- lib/Fetcher.py
+++ lib/Fetcher.py
@@ -44,12 +44,18 @@ class Fetcher:
         # todo: add console print message here if 'projects' doesn't contain one element - output the 'name' in the message so the user knows which project can't be found
         return projects[0]['phid']
 
+    def __projectNameFromProjectJSON(self, projectJSON):
+        parent = projectJSON['fields']['parent'] if projectJSON['fields']['parent'] != None else None
+        if parent == None:
+            return projectJSON['fields']['name']
+        return f'''{parent['name']} ({projectJSON['fields']['name']})'''
+
     def fetchProjectNamesMatchingSearchTerm(self, searchTerm):
         result = self.fetchJSON('/api/project.search', {
             'api.token' : self.apiToken,
-            'constraints[query]' : searchTerm
+            'constraints[query]' : f'title:"{searchTerm}"'
         })
-        return list(map(lambda x: x['fields']['name'], result['result']['data']))
+        return list(map(lambda projectJSON: self.__projectNameFromProjectJSON(projectJSON), result['result']['data']))
 
     # Prefix 'name' with:
     #   '@' for User
