e6f74ac3db8f886bf63f86dc95d5ee46cb020b8e
montehurd <mhurd@wikimedia.org>
2021-03-23 02:47:32 -0700

    move hydration into webVC

--- lib/WebviewController.py
+++ lib/WebviewController.py
@@ -2,9 +2,9 @@
 
 import Utilities, time, re, json, webview, ButtonManifestRegistry, DataStore, uuid
 from string import Template
-from ProjectsHydrator import ProjectsHydrator
 from ButtonManifest import ButtonManifest
 from Project import Project
+from Column import Column
 
 class WebviewController:
     def __init__(self, window, fetcher):
@@ -122,20 +122,86 @@ class WebviewController:
             columnPHIDsToIgnoreForButtons = DataStore.getConfigurationValue('destinationProject')['ignoreColumns']
         )
 
+    def __ticketStatusAndPriorityMenuHTML(self, ticketID, ticketJSON):
+        return f'''
+            {self.__ticketStatusButtonMenuHTML('Status', ticketID, ticketJSON)}
+            {self.__ticketPriorityButtonMenuHTML('Priority', ticketID, ticketJSON)}
+        '''
+
+    def __fetchColumns(self, project):
+        columnsData = self.fetcher.fetchColumnsData(project.phid)
+        return list(map(lambda columnData: Column(columnData['fields']['name'], project, columnData['phid']), columnsData))
+
+    def __allProjectPHIDs(self):
+        phids = list(map(lambda project: project.phid, self.sourceProjects))
+        if self.destinationProject and self.destinationProject.phid != None:
+            phids.append(self.destinationProject.phid)
+        return phids
+
+    def __allColumnPHIDs(self):
+        allColumnPHIDs = self.destinationProject.columnPHIDsToIgnoreForButtons if self.destinationProject != None else []
+        for project in self.sourceProjects:
+            allColumnPHIDs = allColumnPHIDs + project.columnPHIDs
+        return allColumnPHIDs
+
+    def hydrateProjects(self, hydrateTickets = True):
+        # fetch destination project names and columns
+
+        # hydrate source and destination project and column names from configuration.json phids
+        self.__setLoadingMessage('Fetching project and column names')
+        openItemNamesByPHID = self.fetcher.fetchNamesForStatusOpenPHIDs(self.__allProjectPHIDs() + self.__allColumnPHIDs())
+
+        for sourceProject in self.sourceProjects:
+            sourceProject.name = openItemNamesByPHID[sourceProject.phid]
+        if self.destinationProject != None:
+            self.destinationProject.name = openItemNamesByPHID[self.destinationProject.phid]
+
+        addToDestinationColumnMenuHTMLLambdas = []
+
+        if self.destinationProject != None:
+            self.__setLoadingMessage(f"Fetching '{self.destinationProject.name}' columns")
+            self.destinationProject.buttonsMenuColumns = self.__fetchColumns(self.destinationProject)
+
+            destinationColumns = list(filter(lambda column: (column.phid not in self.destinationProject.columnPHIDsToIgnoreForButtons), self.destinationProject.buttonsMenuColumns))
+            if len(destinationColumns) > 0:
+                addToDestinationColumnMenuHTMLLambdas.append(
+                    lambda ticketID, ticketJSON, columns=destinationColumns :
+                        self.__ticketAddToColumnButtonMenuHTML(f'Add to column on destination project ( <i>{self.destinationProject.name}</i> )', ticketID, ticketJSON, columns)
+                )
+
+        for project in self.sourceProjects:
+            # fetch source project columns
+            self.__setLoadingMessage(f"Fetching '{project.name}' columns")
+            project.buttonsMenuColumns = self.__fetchColumns(project)
+
+            currentSourceColumnMenuHTMLLambda = [
+                lambda ticketID, ticketJSON, columns=project.buttonsMenuColumns :
+                    self.__ticketAddToColumnButtonMenuHTML(f'Current column on source project ( <i>{project.name}</i> )', ticketID, ticketJSON, columns)
+            ]
+
+            # make column object for each column name
+            project.columns = list(map(lambda columnPHID: Column(name = openItemNamesByPHID[columnPHID], project = project, phid = columnPHID), project.columnPHIDs))
+
+            for column in project.columns:
+                self.__setLoadingMessage(f"Fetching '{project.name} > {column.name}' tickets")
+                if not hydrateTickets: # if only reloading the configuration UI the ticket hydration below is not needed
+                    continue
+                tickets = list(self.fetcher.fetchColumnTickets(column.phid))
+                # dict with ticketID as key and ticketJSON as value (excluding tickets already tagged with destinationProjectPHID)
+                ticketsByID = dict((x['id'], x) for x in tickets if self.destinationProject == None or not self.destinationProject.phid in x['attachments']['projects']['projectPHIDs'])
+                column.ticketsByID = ticketsByID
+                # print(json.dumps(self.ticketsByID, indent=4))
+                column.tickets = column.ticketsByID.values()
+                column.menuHTMLLambdas = currentSourceColumnMenuHTMLLambda + column.menuHTMLLambdas + addToDestinationColumnMenuHTMLLambdas + [self.__ticketStatusAndPriorityMenuHTML]
+                # fetch column tickets html for their remarkup
+                self.__setLoadingMessage(f"Fetching '{project.name} > {column.name}' tickets html")
+                column.ticketsHTMLByID = self.fetcher.fetchTicketsHTMLByID(column.tickets)
+
     def __load(self, hydrateTickets = True):
         self.sourceProjects = self.__getDehydratedSourceProjects()
         self.destinationProject = self.__getDehydratedDestinationProject()
         self.__setLoadingMessage('Beginning data retrieval')
-        ProjectsHydrator(
-            sourceProjects = self.sourceProjects,
-            destinationProject = self.destinationProject,
-            fetcher = self.fetcher,
-            loadingMessageSetter = self.__setLoadingMessage,
-            ticketAddToColumnButtonMenuHTMLFunction = self.__ticketAddToColumnButtonMenuHTML,
-            ticketStatusButtonMenuHTMLFunction = self.__ticketStatusButtonMenuHTML,
-            ticketPriorityButtonMenuHTMLFunction = self.__ticketPriorityButtonMenuHTML
-        ).hydrateProjects(hydrateTickets = hydrateTickets)
-
+        self.hydrateProjects(hydrateTickets = hydrateTickets)
         self.__setLoadingMessage('')
 
         # can start html generation now that projects are hydrated
