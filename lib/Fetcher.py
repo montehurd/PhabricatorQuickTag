a7759f01de93d75cd45a782b522973352e28ec13
montehurd <mhurd@wikimedia.org>
2021-04-23 14:39:11 -0700

    limit project search results to projects and tags
    in preparation for adding support for 'tag' ticket sources

--- lib/Fetcher.py
+++ lib/Fetcher.py
@@ -51,7 +51,9 @@ class Fetcher:
     def fetchProjectsMatchingSearchTerm(self, searchTerm):
         result = self.fetchJSON('/api/project.search', {
             'api.token' : self.apiToken,
-            'constraints[query]' : f'title:"{searchTerm}"'
+            'constraints[query]' : f'title:"{searchTerm}"',
+            'constraints[icons][0]' : 'project',
+            'constraints[icons][1]' : 'tag'
         })
         if result['result'] == None:
             return []
